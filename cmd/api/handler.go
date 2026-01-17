b3f7a43c1ddb70fde654740372203124f8563b3c
pradeek <pradeek.k@gmail.com>
2026-01-17 18:48:40 +0530

    implemented json related utils - writejson, readjson, errorjson for writing error as json

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -18,30 +15,16 @@ func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
 		Version: "1.0.0",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
 func (app *Application) AllMovies(w http.ResponseWriter, r *http.Request) {
 
 	movies, err := app.DB.AllMovies()
 	if err != nil {
-		fmt.Println(err)
+		app.errorJSON(w, err)
 		return
 	}
 
-	out, err := json.Marshal(movies)
-	if err != nil {
-		log.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, movies)
 }
