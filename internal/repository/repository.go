7df4cc8266d4dd7b570b663efd400cc8b442fc34
pradeek <pradeek.k@gmail.com>
2026-01-30 01:13:42 +0530

    025_10 completed

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -14,7 +14,7 @@ type DatabaseRepo interface {
 	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
 	OneMovie(id int) (*models.Movie, error)
 	AllGenres() ([]*models.Genre, error)
-
 	InsertMovie(movie models.Movie) (int, error)
 	UpdateMovieGenres(id int, genreIDs []int) error
+	UpdateMovie(movie models.Movie) error
 }
